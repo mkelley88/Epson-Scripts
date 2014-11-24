a6286d3ff8d1f342a5d5e51f2e3e3aaed9216420
Matthew T. Kelley <mkelley88@gmail.com>
2014-11-24 04:42:58 -0500

    Fixed problem when resizing images

--- img2pos.py
+++ img2pos.py
@@ -5,11 +5,11 @@
 @copyright: Copyright (c) 2014 Matthew T Kelley
 @license: GPL
 
- - I am using Pillow to provide the Python Imaging Library (PIL)
- - Written and tested under Python 2.7
- - Code requires modifications to escpos/constants.py
+ Using Pillow to provide the Python Imaging Library (PIL)
+ Written and tested under Python 2.7
+ Code requires modifications to escpos/constants.py
  Python-ESCPOS may be obtained from these sources:
-     Original: https://code.google.com/p/python-escpos
+     Original: https://code.google.com/p/python-escpos/
      My fork: https://github.com/mkelley88/python-escpos
  My fork is highly recommended and may be requried to use my scripts.
 '''
@@ -22,9 +22,8 @@ import sys
 # Setup serial object. Set your port and baudrate here!
 Epson = printer.Serial("/dev/ttyUSB0",38400)
 
-# Will be used to change baud rate on TM-T88IV and newer printers
-# Epson.setPrintSpeed("57600")
-# Epson = printer.Serial("/dev/ttyUSB1",57600)
+#Epson.setPrintSpeed("57600")
+#Epson = printer.Serial("/dev/ttyUSB1",57600)
 
 # Try to open image file provided at command line:
 for infile in sys.argv[1:]:
@@ -38,7 +37,11 @@ for infile in sys.argv[1:]:
 # This calculates the new image size to be sent to the function printIt()
 if im.size[0] > 512:
     imgWidth = 512
-    imgHeight = int(imgWidth * im.size[0] / float(im.size[1]))
+    if im.size[1] > im.size[0]:
+        imgHeight = int(imgWidth * (im.size[0] / float(im.size[1])))
+    else:
+        imgHeight = int(imgWidth * (im.size[1] / float(im.size[0])))
+
 else:
     imgWidth = im.size[0]
     imgHeight = im.size[1]
@@ -53,24 +56,25 @@ def printIt(width, height):
         out = im.resize((im.size[0],im.size[1]))
 
     print ("Height = %d" % height)
-    for y in range(0, height):
+    for y in range(0, height): 
         if (y*255+255) > height:
             box = (0, y*255, width, height)
             brk = 1
-        else:
+    	else: 
             box = (0, y*255, width, y*255+255 )
-        print (box)
-        out1 = out.crop(box)
-        out1.save("out1.png", "PNG")
-        Epson.image("out1.png")
-        y += 255
+	print (box)    
+	out1 = out.crop(box)
+    	out1.save("out1.png", "PNG")
+    	Epson.image("out1.png")
+	y += 255
         if brk != 0:
             break
+                     
     Epson.cut()
 
-print(im.size[0],"x",im.size[1])  # DEBUG # These lines may be removed
+print(im.size[0],"x",im.size[1])  # DEBUG #
 print(imgWidth,"x",imgHeight)     # DEBUG #
-printIt(imgWidth,imgHeight)       # Keep this line!
+printIt(imgWidth,imgHeight)
 
 # TO DO #
 '''
@@ -78,3 +82,4 @@ printIt(imgWidth,imgHeight)       # Keep this line!
     Clean up messy code
     Make usable as a library?
 '''
+ 
